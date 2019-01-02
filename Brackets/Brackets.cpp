2a75408f1dada515f4142b4af4ddf30b8ea5a113
IevgenTolstikhin <ugine90@mail.ru>
2019-01-02 19:31:02 +0100

    Add files via upload

--- Brackets/Brackets.cpp
+++ Brackets/Brackets.cpp
@@ -1,64 +1,95 @@
+#include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <unordered_map>
+#include <unordered_set>
 #include <stack>
-#include <set>
-#include <map>
-#include <algorithm>
+#include <functional>
 
-int main(int argc, char** argv)
+int
+main( int argc, char** argv )
 {
-	if (argc != 2)
-	{
-		std::cout << "Invalid amount of arguments" << std::endl;
-		return 1;
-	}
+    if ( argc != 2 )
+    {
+        std::cout << "Invalid amount of arguments" << std::endl;
+        return 1;
+    }
 
-	std::string input(argv[1]);
-	std::unordered_map<char, char> dict	//It can be read from some config file
-	{
-		std::make_pair('}', '{'), 
-		std::make_pair(']', '['), 
-		std::make_pair(')', '(')
-	};
+    std::string input( argv[ 1 ] );
 
-	std::unordered_set<char> dict_start;
+    enum class RetCode : int
+    {
+        INVALID_SEQUENCE,
+        NOT_A_BRACKET,
+        NEED_STACK_PUSH,
+        NEED_STACK_POP
+    };
 
-	std::transform(dict.begin(), dict.end(), std::inserter(dict_start, dict_start.begin()),
-    	[](const std::pair<char, char>& key_value) {
-        	return key_value.second;
-   	});
+    std::unordered_map< char, std::function< RetCode( const std::stack< char >&, const char ) > >
+        reactionTable;
+    std::unordered_map< char, char > dict  // It can be read from some config file
+        {std::make_pair( '}', '{' ), std::make_pair( ']', '[' ), std::make_pair( ')', '(' )};
 
-    	std::cout << "Trace: ";
-    	std::copy(dict_start.begin(), dict_start.end(),
-              	std::ostream_iterator<char>(std::cout, " "));
-    	std::cout << std::endl;
+    std::unordered_set< char > dict_start;
 
-	std::stack<char> st;
-	for (auto& c : input)
-	{
-		if (dict.find(c) != dict.end() && st.empty())
-		{
-			std::cout << "Invalid sequence of brackets" << std::endl;
-			return 2;
-		} 
+    std::transform( dict.begin( ), dict.end( ), std::inserter( dict_start, dict_start.begin( ) ),
+                    []( const std::pair< char, char >& key_value ) { return key_value.second; } );
 
-		if (dict_start.find(c) != dict_start.end())
-		{
-			st.push(c);
-			continue;
-		}
+    // Use only ASCII table
+    reactionTable.reserve( 128 );
+    for ( char symbol = 0; symbol < 127; ++symbol )
+    {
+        reactionTable[ symbol ]
+            = [&dict_start, &dict]( const std::stack< char >& st, const char symbol ) {
+                  if ( dict.find( symbol ) != dict.end( ) )
+                  {
+                      if ( st.empty( ) )
+                      {
+                          return RetCode::INVALID_SEQUENCE;
+                      }
+                      else
+                      {
+                          if ( dict.at( symbol ) == st.top( ) )
+                          {
+                              return RetCode::NEED_STACK_POP;
+                          }
+                          else
+                          {
+                              return RetCode::INVALID_SEQUENCE;
+                          }
+                      }
+                  }
 
-		if (dict.find(c) != dict.end() && !st.empty())
-		{
-			if (dict.at(c) == st.top())
-				st.pop();
-			else
-			{
-				std::cout << "Invalid sequence of brackets" << std::endl;
-				return 3;
-			}
-		}
-	}
+                  if ( dict_start.find( symbol ) != dict_start.end( ) )
+                  {
+                      return RetCode::NEED_STACK_PUSH;
+                  }
 
-	return 0;
+                  return RetCode::NOT_A_BRACKET;
+              };
+    }
+
+    std::stack< char > st;
+    for ( const auto& symbol : input )
+    {
+        switch ( reactionTable[ symbol ]( st, symbol ) )
+        {
+        case RetCode::INVALID_SEQUENCE:
+            std::cout << "Invalid sequence of brackets" << std::endl;
+            return 2;
+        case RetCode::NEED_STACK_PUSH:
+            st.push( symbol );
+            break;
+        case RetCode::NEED_STACK_POP:
+            st.pop( );
+            break;
+        case RetCode::NOT_A_BRACKET:
+        default:
+            break;
+        }
+    }
+
+    std::cout << "All brackets are in correct sequence" << std::endl;
+
+    return 0;
 }
---
+++ Brackets/README.txt
@@ -0,0 +1,3 @@
+This sample project calculate if the opened brackets are properly closed. Used only ASCII symbols.
+
+TODO: Add tests
